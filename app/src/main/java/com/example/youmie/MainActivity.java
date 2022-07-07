0bba5c0cd1c5e52ee673de04721f08c1aadb353d
alodijutgla <allodi.serrat@bitmovin.com>
2022-07-07 20:24:36 +0200

    Adde home page and with animation

--- app/src/main/java/com/example/youmie/MainActivity.java
+++ app/src/main/java/com/example/youmie/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.youmie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.imageTitle);
+        runAnimation(imageView);
+    }
+
+    private void runAnimation(ImageView imageView) {
+        ScaleAnimation fade_in = new ScaleAnimation(0f, 1f, 0f, 1f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        fade_in.setDuration(1500);
+
+        ScaleAnimation fade_out = new ScaleAnimation(1f, 0f, 1f, 0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        fade_out.setDuration(1500);
+
+        imageView.startAnimation(fade_in);
+        fade_in.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imageView.startAnimation(fade_out);
+            }
+        });
     }
 }
