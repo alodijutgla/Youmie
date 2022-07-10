58f50dc93ca5200e6495b391e8a7a03a2dbff164
alodijutgla <allodi.serrat@bitmovin.com>
2022-07-10 11:06:05 +0200

    add database part for sign up

---
+++ app/src/main/java/com/example/youmie/DatabaseUtils.java
@@ -0,0 +1,51 @@
+package com.example.youmie;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseUtils extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "UsersDB.db";
+
+    public DatabaseUtils(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase MyDB) {
+        MyDB.execSQL("create Table users(username TEXT primary key, password TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase MyDB, int i, int i1) {
+        MyDB.execSQL("drop Table if exists users");
+    }
+
+    public boolean verifyUsername(String username) {
+        SQLiteDatabase liteDatabase = this.getReadableDatabase();
+        try (Cursor cursor = liteDatabase.rawQuery("Select * from users where username = ?", new String[]{username})) {
+            return cursor.getCount() > 0;
+        }
+    }
+
+    public boolean checkIfNonUniquePwd(String pwd) {
+        SQLiteDatabase liteDatabase = this.getReadableDatabase();
+        try (Cursor cursor = liteDatabase.rawQuery("Select * from users where password = ?", new String[]{pwd})) {
+            return cursor.getCount() > 0;
+        }
+    }
+
+    public boolean insertUser(String username, String password) {
+        SQLiteDatabase liteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("username", username);
+        contentValues.put("password", password);
+        long result = liteDatabase.insert("users", null, contentValues);
+        return result != -1;
+    }
+
+}
--- app/src/main/java/com/example/youmie/RegisterActivity.java
+++ app/src/main/java/com/example/youmie/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.youmie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,7 @@ public class RegisterActivity extends AppCompatActivity {
         EditText password = (EditText) findViewById(R.id.password_reg);
         EditText passwordRep = (EditText) findViewById(R.id.password_rep_reg);
         Button signUp = (Button) findViewById(R.id.signUpBtn);
+        DatabaseUtils databaseUtils = new DatabaseUtils(this);
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +36,19 @@ public class RegisterActivity extends AppCompatActivity {
                 } else {
                     if (!passText.equals(passRepText)) {
                         Toasty.error(RegisterActivity.this, "Please make sure your passwords match", Toast.LENGTH_SHORT, true).show();
+                    } else {
+                        if (databaseUtils.verifyUsername(userText)) {
+                            Toasty.warning(RegisterActivity.this, "Sorry but this username is already taken ;)", Toast.LENGTH_SHORT, true).show();
+                        } else if (databaseUtils.checkIfNonUniquePwd(passText)) {
+                            Toasty.warning(RegisterActivity.this, "This password is not available, please try again", Toast.LENGTH_LONG, true).show();
+                        } else {
+                            if (databaseUtils.insertUser(userText, passText)) {
+                                Toasty.success(RegisterActivity.this, "Registered successfully", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(), WelcomeActivity.class));
+                            } else {
+                                Toasty.error(RegisterActivity.this, "Registration failed", Toast.LENGTH_SHORT, true).show();
+                            }
+                        }
                     }
                 }
             }
